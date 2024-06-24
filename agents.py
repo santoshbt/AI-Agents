9100df014a2f0eeed3ce7b35088d5b732430abac
llssbb <bt.santosh01@gmail.com>
2024-06-24 22:29:20 +0100

    added agents and tasks

--- agents.py
+++ agents.py
@@ -7,3 +7,29 @@ load_dotenv()
 import os
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 os.environ["OPENAI_MODEL_NAME"] = "gpt-4-0125-preview"
+
+blog_researcher=Agent(
+    role='Blog Researcher from Youtube Videos',
+    goal='get the relevant video transcription for the topic {topic} from the provided Yt channel',
+    verbose=True,
+    memory=True,
+    backstory=(
+       "Expert in understanding videos in AI Data Science , MAchine Learning And GEN AI and providing suggestion"
+    ),
+    tools=[yt_tool],
+    allow_delegation=True
+)
+
+blog_writer=Agent(
+    role='Blog Writer',
+    goal='Narrate compelling tech stories about the video {topic} from YT video',
+    verbose=True,
+    memory=True,
+    backstory=(
+        "With a flair for simplifying complex topics, you craft"
+        "engaging narratives that captivate and educate, bringing new"
+        "discoveries to light in an accessible manner."
+    ),
+    tools=[yt_tool],
+    allow_delegation=False
+)
--- crew.py
+++ crew.py
@@ -1,10 +1,17 @@
-from crewai import crew, Process
+from crewai import Crew, Process
+from agents import blog_researcher, blog_writer
+from task import research, write
 
 
 crew = Crew(
-
+  agents=[blog_researcher, blog_writer],
+  tasks=[research, write],
+  process=Process.sequential,  # Optional: Sequential task execution is default
+  memory=True,
+  cache=True,
+  max_rpm=100,
+  share_crew=True
 )
 
-result = Crew.kickoff(input = {''})
-
+result=crew.kickoff(inputs={'topic':'Realtime Multimodal RAG Usecase Part 3 | MultiVectorRetriever with Langchain | RAG Application'})
 print(result)
